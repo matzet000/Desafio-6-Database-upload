ab928f7b49f111ac43c0505dd184e39dd7462756
matzet99 <matheus.zeitune.developer@gmail.com>
2020-08-29 23:53:44 -0300

    Desafio finalizado

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 import multer from 'multer';
 
+import uploadConfig from '../config/upload';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
 import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
+const upload = multer(uploadConfig);
 
 transactionsRouter.get('/', async (request, response) => {
   const transactionsRepository = getCustomRepository(TransactionsRepository);
@@ -40,8 +42,18 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.status(204).send();
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  return response.send();
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const { path } = request.file;
+
+    const importTransactions = new ImportTransactionsService();
+
+    const transactions = await importTransactions.execute(path);
+
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
